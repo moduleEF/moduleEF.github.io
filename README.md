fcd0e5de2364ab7b4375fec2ecefd156c5f98ba5
exequieldelaguia <exequieldelaguia@gmail.com>
2021-12-03 10:03:02 -0300

    added contact form

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 
 ## TODO LIST
 
+- [x] Footer
+    - [x] Telefono
+    - [x] Email
+- [ ] Contact Form
+    - [ ] Crear Cuenta Formspree
+    - [ ] Guardar Datos Formspree
+    - [ ] Modificar Script
+    - [ ] Frontend Contact Form   
+- [ ] Main content
+    - [ ] Header
+    - [ ] Quienes Somos
+    - [ ] Contacto
+- [ ] Icons
 - [ ] Navbar
-- [x] Help
--- contact.js
+++ contact.js
@@ -1,31 +1,3 @@
-navbar = document.querySelector('.navbar');
-hamburgerMenu = document.querySelector('.hamburger-menu');
-navbarBtn = document.querySelector('.navbar-btn');
-hamburgerMenuCloseBtn = document.querySelector('.hamburger-menu ul li:nth-child(1) img');
-
-navbarBtn.addEventListener('click', () => {
-    toggleHamburgerMenu();
-})
-hamburgerMenuCloseBtn.addEventListener('click', () => {
-    toggleHamburgerMenu();
-})
-
-function toggleHamburgerMenu() {
-    navbar.classList.toggle('navbar-active');
-    hamburgerMenu.classList.toggle('open-navbar');
-}
-
-
-for (let x = 0; x < hamburgerMenu.children[0].children.length; x++) {
-    let item = hamburgerMenu.children[0].children[x].children[0];
-    let nodeName = item.nodeName;
-    if (nodeName == 'A') {
-        item.addEventListener('click', () => {
-            toggleHamburgerMenu();
-        })
-    }
-}
-
 let form = document.querySelector("#form");
 let inputs = document.querySelectorAll('input');
 
